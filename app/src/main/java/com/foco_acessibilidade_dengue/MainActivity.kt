ebaa1433044bc6ae86f6ca39880f0e52604be739
Luciano <lucianoggr471@gmail.com>
2026-05-21 14:56:10 -0300

    Alteração teste

--- app/src/main/java/com/foco_acessibilidade_dengue/MainActivity.kt
+++ app/src/main/java/com/foco_acessibilidade_dengue/MainActivity.kt
@@ -33,7 +33,7 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
     Text(
-        text = "Miguel $name!",
+        text = "Oi $name!",
         modifier = modifier
     )
 }
---
+++ app/src/main/java/com/foco_acessibilidade_dengue/teste.kt
@@ -0,0 +1,5 @@
+package com.foco_acessibilidade_dengue
+
+fun funcao() {
+
+}
