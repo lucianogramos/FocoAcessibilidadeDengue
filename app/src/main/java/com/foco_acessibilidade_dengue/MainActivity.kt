313f75b27d80144fcc3d17320a9b735607383578
Luciano <lucianoggr471@gmail.com>
2026-05-21 13:53:34 -0300

    Initial commit

---
+++ app/src/main/java/com/foco_acessibilidade_dengue/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.foco_acessibilidade_dengue
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.foco_acessibilidade_dengue.ui.theme.FocoAcessibilidadeDengueTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            FocoAcessibilidadeDengueTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    FocoAcessibilidadeDengueTheme {
+        Greeting("Android")
+    }
+}
